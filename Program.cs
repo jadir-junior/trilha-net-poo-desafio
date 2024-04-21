f9b72fda86552fff48dd8a9f236fa136335245e1
Jadir Junior <jadirjsjunior@gmail.com>
2024-04-21 13:50:43 -0300

    feito herança e criado construtor e feito polimorfismo no metodo de instalarAplicativo para a classe Nokia e Iphone Feito os testes no arquivo Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("+5511711760148", "Nokia C20", "352391982894641", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("+5511483577630", "14", "443393287861821", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
